9c024d82b23528fe110e9b164f9f9f02cafa3caa
Ashik <ashikes@gmail.com>
2014-01-18 20:55:28 +0530

    Mongo Spark and FreeMarker test together.

---
+++ src/main/java/com/blog/HelloWorldMongoDBSparkFreeMarkerStyle.java
@@ -0,0 +1,47 @@
+package com.blog;
+
+import com.mongodb.*;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+import java.io.StringWriter;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ashik on 1/18/14 8:43 PM.
+ */
+public class HelloWorldMongoDBSparkFreeMarkerStyle {
+
+    public static void main(String args[]) throws UnknownHostException {
+
+        final Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(HelloWorldSparkStyle.class, "/");
+
+        MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
+        final DB database = client.getDB("test");
+
+        Spark.get(new Route("/") {
+            @Override
+            public Object handle(Request request, Response response) {
+                StringWriter writer = new StringWriter();
+                try {
+                    Template template = configuration.getTemplate("hello.ftl");
+                    DBCollection collection = database.getCollection("names");
+                    DBObject document = collection.findOne();
+
+                    template.process(document, writer);
+                } catch (Exception e) {
+                    halt(500);
+                    e.printStackTrace();
+                }
+                return writer;
+            }
+        });
+    }
+}
