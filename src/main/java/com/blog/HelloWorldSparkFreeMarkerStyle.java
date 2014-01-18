e593fc81601bd5021702f68ce3ab8d8967b9e474
Ashik <ashikes@gmail.com>
2014-01-18 20:38:26 +0530

    Spark and FreeMarker together.

---
+++ src/main/java/com/blog/HelloWorldSparkFreeMarkerStyle.java
@@ -0,0 +1,46 @@
+package com.blog;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ashik on 1/18/14 8:32 PM.
+ */
+public class HelloWorldSparkFreeMarkerStyle {
+
+    public static void main(String args[]) {
+
+        final Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(HelloWorldSparkStyle.class, "/");
+
+        Spark.get(new Route("/") {
+            @Override
+            public Object handle(Request request, Response response) {
+                StringWriter writer = new StringWriter();
+                try {
+                    Template template = configuration.getTemplate("hello.ftl");
+                    Map<String, Object> helloMap = new HashMap<String, Object>();
+                    helloMap.put("name", "FreeMarker");
+
+                    template.process(helloMap, writer);
+                } catch (Exception e) {
+                    halt(500);
+                    e.printStackTrace();
+                }
+                return writer;
+            }
+        });
+    }
+
+
+}
